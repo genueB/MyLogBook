96bddc4d7b88a8f9430e83f49fed196f1f9bc2ef
genueB <bay9895@gmail.com>
2022-12-24 20:46:05 +0900

    Initial Commit

---
+++ MyLogBook/MyLogBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLogBookApp.swift
+//  MyLogBook
+//
+//  Created by 배진우 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyLogBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
