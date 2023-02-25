8324ace13252ddf2699dffa896c14d4f2e131858
genueB <bay9895@gmail.com>
2023-02-25 09:52:22 +0900

    [ADD] 로그 생성 화면 추가

---
+++ MyLogBook/AddLogView.swift
@@ -0,0 +1,23 @@
+//
+//  AddLogView.swift
+//  MyLogBook
+//
+//  Created by 배진우 on 2023/02/20.
+//
+
+import SwiftUI
+
+struct AddLogView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Text("AddLogView!")
+        }
+        .frame(maxHeight: .infinity)
+    }
+}
+
+struct AddLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddLogView()
+    }
+}
